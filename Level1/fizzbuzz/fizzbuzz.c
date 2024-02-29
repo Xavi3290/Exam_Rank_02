2fc84f15a842a6752411d03f278721176305c4dd
Xavier Roca Perez <xroca-pe@cbr3s1.42barcelona.com>
2024-02-29 17:07:36 +0100

    update

--- Level1/fizzbuzz/fizzbuzz.c
+++ Level1/fizzbuzz/fizzbuzz.c
@@ -19,12 +19,12 @@ void fizzbuzz()
 	i = 1;
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
 			write(1, "fizz", 4);
 		else if (i % 5 == 0)
 			write(1, "buzz", 4);
-		else if (i % 3 == 0 && i % 5 == 0)
-			write(1, "fizzbuzz", 8);
 		else
 			ft_putnbr(i);
 		write(1, "\n", 1);
